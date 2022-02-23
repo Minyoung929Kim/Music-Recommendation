c42223cf6f7496a28d2f2330c1738aa0efe164dc
syncdoth <choisehyun98@gmail.com>
2022-02-23 20:33:47 +0900

    update run.py

--- run.py
+++ run.py
@@ -19,14 +19,14 @@ fout.close()
 # get importance scores
 
 importance_scores = data_class.get_importance_score('survey_data/importance.csv')
+maximum_depression_score = importance_scores[[
+    i - 1 for i in data_class.continuous_questions  # [3, 2, 1] -> [2, 1, 0]
+]].sum()
+
 depression_scores = data_class.get_depression_score(importance_scores)  # [102,]
 
-# TODO: change the threshold
-# 17 continuous questions, each with 1~5, and importance is also 1~5
-# in theory, the maximum score can be: 5 * 5 * 17 = 425
-# in theory, the minimum score can be: 17
-
-depression_threshold = (importance_scores * 3).sum()
+# NOTE: decided the threshold to be 14 after the meeting (Feb. 23, 2022)
+depression_threshold = (14 / 17) * maximum_depression_score
 
 depressed_label = depression_scores > depression_threshold  # [102,]
-np.save('survey_data/depressed_label.npy', depressed_label)
+np.save('survey_data/depression_label.npy', depressed_label)
